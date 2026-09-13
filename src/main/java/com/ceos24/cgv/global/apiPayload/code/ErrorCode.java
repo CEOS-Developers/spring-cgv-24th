2544e2f72659140bbd1c66ed45968974e1fafd7d
taiho0303 <taiho0303@gmail.com>
2026-09-13 19:13:30 +0900

    feat: 공통 응답 형식 및 전역 예외 처리 구현

---
+++ src/main/java/com/ceos24/cgv/global/apiPayload/code/ErrorCode.java
@@ -0,0 +1,79 @@
+package com.ceos24.cgv.global.apiPayload.code;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+
+    /**
+     * ******************************* Global Error CodeList ***************************************
+     * HTTP Status Code
+     * 400 : Bad Request
+     * 401 : Unauthorized
+     * 403 : Forbidden
+     * 404 : Not Found
+     * 500 : Internal Server Error
+     * *********************************************************************************************
+     */
+    // 잘못된 서버 요청
+    BAD_REQUEST_ERROR(400, "G001", "Bad Request Exception"),
+
+    // @RequestBody 데이터 미 존재
+    INVALID_REQUEST_BODY(400, "G002", "요청 본문이 없거나 형식이 올바르지 않습니다."),
+
+    // 유효하지 않은 타입
+    INVALID_TYPE_VALUE(400, "G003", " Invalid Type Value"),
+
+    // Request Parameter 로 데이터가 전달되지 않을 경우
+    MISSING_REQUEST_PARAMETER_ERROR(400, "G004", "Missing Servlet RequestParameter Exception"),
+
+    // 입력/출력 값이 유효하지 않음
+    IO_ERROR(400, "G005", "I/O Exception"),
+
+    // com.google.gson JSON 파싱 실패
+    JSON_PARSE_ERROR(400, "G006", "JsonParseException"),
+
+    // com.fasterxml.jackson.core Processing Error
+    JACKSON_PROCESS_ERROR(400, "G007", "com.fasterxml.jackson.core Exception"),
+
+    // 권한이 없음
+    FORBIDDEN_ERROR(403, "G008", "Forbidden Exception"),
+
+    // 서버로 요청한 리소스가 존재하지 않음
+    NOT_FOUND_ERROR(404, "G009", "Not Found Exception"),
+
+    // NULL Point Exception 발생
+    NULL_POINT_ERROR(500, "G010", "Internal Server Error"),
+
+    // @RequestBody 및 @RequestParam, @PathVariable 값이 유효하지 않음
+    NOT_VALID_ERROR(400, "G011", "요청 값이 유효하지 않습니다."),
+
+    // @RequestBody 및 @RequestParam, @PathVariable 값이 유효하지 않음
+    NOT_VALID_HEADER_ERROR(400, "G012", "필수 요청 헤더가 없습니다."),
+
+    METHOD_NOT_ALLOWED(405, "G013", "지원하지 않는 HTTP 메서드입니다."),
+
+    UNSUPPORTED_MEDIA_TYPE(415, "G014", "지원하지 않는 Content-Type입니다."),
+
+    // 서버가 처리 할 방법을 모르는 경우 발생
+    INTERNAL_SERVER_ERROR(500, "G999", "Internal Server Error Exception");
+
+    /**
+     * ******************************* Error Code Constructor ***************************************
+     */
+    // 에러 코드의 '코드 상태'을 반환한다.
+    private final int status;
+
+    // 에러 코드의 '코드간 구분 값'을 반환한다.
+    private final String divisionCode;
+
+    // 에러 코드의 '코드 메시지'을 반환한다.
+    private final String message;
+
+    // 생성자 구성
+    ErrorCode(final int status, final String divisionCode, final String message) {
+        this.status = status;
+        this.divisionCode = divisionCode;
+        this.message = message;
+    }
+}
