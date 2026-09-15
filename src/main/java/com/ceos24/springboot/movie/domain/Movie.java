96ef0dd0dcf71ea08c827a559e2b95890bc3ceac
uoahns23 <uoow.23@gmail.com>
2026-09-16 02:37:14 +0900

    feat/ Movie 도메인 및 레포 구현

---
+++ src/main/java/com/ceos24/springboot/movie/domain/Movie.java
@@ -0,0 +1,43 @@
+package com.ceos24.springboot.movie.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Table(name = "movie")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "movie_id")
+    private Long movieId;
+
+    @Column(name = "title_kr")
+    private String titleKr;
+
+    @Column(name = "title_en")
+    private String titleEn;
+
+    @Column(name = "runtime_minutes")
+    private Integer runtimeMinutes;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "age_rating", length = 10)
+    private AgeRating ageRating;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+    @Column(name = "director")
+    private String director;
+
+
+    @Column(name = "cast_members", length = 255)
+    private String castMembers;
+}
