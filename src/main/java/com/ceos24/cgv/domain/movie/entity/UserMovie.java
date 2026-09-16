f93ebea841e5d5eea5efe1a2c769f4e6f89c9bb1
박서영 <emilypark0721@gmail.com>
2026-09-16 12:47:13 +0900

    feat: 영화 찜 관련 api 구현 완료

--- src/main/java/com/ceos24/cgv/domain/movie/entity/UserMovie.java
+++ src/main/java/com/ceos24/cgv/domain/movie/entity/UserMovie.java
@@ -1,4 +1,49 @@
 package com.ceos24.cgv.domain.movie.entity;
 
-public class UserMovie {
+import com.ceos24.cgv.domain.user.entity.User;
+import com.ceos24.cgv.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        name = "user_movies",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_user_movie",
+                        columnNames = {
+                                "user_id",
+                                "movie_id"
+                        }
+                )
+        }
+)
+public class UserMovie extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_movie_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id", nullable = false)
+    private Movie movie;
+
+    @Builder
+    private UserMovie(
+            User user,
+            Movie movie
+    ) {
+        this.user = user;
+        this.movie = movie;
+    }
 }
---
+++ src/main/java/com/ceos24/cgv/domain/movie/exception/UserMovieErrorCode.java
@@ -0,0 +1,27 @@
+package com.ceos24.cgv.domain.movie.exception;
+
+import com.ceos24.cgv.global.code.BaseErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum UserMovieErrorCode implements BaseErrorCode {
+
+    MOVIE_ALREADY_LIKED(
+            HttpStatus.CONFLICT,
+            "USER_MOVIE409",
+            "이미 찜한 영화입니다."
+    ),
+
+    MOVIE_LIKE_NOT_FOUND(
+            HttpStatus.NOT_FOUND,
+            "USER_MOVIE404",
+            "찜하지 않은 영화입니다."
+    );
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
