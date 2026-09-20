a2abbccd22e255abf4a274e4e99af95ff768457c
zzulYH-11 <jjulu0227@naver.com>
2026-09-20 12:41:46 +0900

    feat: CustomerUserDetailService 구현

--- src/main/java/com/ceos24/cgv/domain/member/repository/MemberRepository.java
+++ src/main/java/com/ceos24/cgv/domain/member/repository/MemberRepository.java
@@ -3,4 +3,8 @@ package com.ceos24.cgv.domain.member.repository;
 import com.ceos24.cgv.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {}
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByLonginId(String longinId);
+}
